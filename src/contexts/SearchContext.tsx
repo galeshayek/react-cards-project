bad010a1fed1821baa4a52db04007f95a9bc1cba
galeshayek <galeshayek15@gmail.com>
2024-04-11 22:19:33 +0300

    deploy

--- src/contexts/SearchContext.tsx
+++ src/contexts/SearchContext.tsx
@@ -3,7 +3,7 @@ import { FCC } from "../@types/types";
 
 export const SearchContext = createContext({
     searchValue: "",
-    handleSearch: (search: string) => { }
+    handleSearch: (search: any) => { }
 });
 
 
