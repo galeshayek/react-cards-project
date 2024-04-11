4914d851455266416edf782ae7125faab863b26c
galeshayek <galeshayek15@gmail.com>
2024-04-11 22:37:33 +0300

    b

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-cards-project/",
   plugins: [react()],
-  base: "/react-cards-project/"
 })
